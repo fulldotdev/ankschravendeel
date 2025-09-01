7c4a2083670a2f19626b511f6595b29a744e2253
Sil <silveltman@gmail.com>
2025-09-01 19:17:53 +0200

    event sorting

--- src/blocks/events-1.tsx
+++ src/blocks/events-1.tsx
@@ -2,6 +2,7 @@ import type { BlockProps } from "@/schemas/block"
 
 import { Heading } from "@/components/ui/heading"
 import { Paragraph } from "@/components/ui/paragraph"
+import { Tagline } from "@/components/ui/tagline"
 import { Writeup } from "@/components/ui/writeup"
 
 export interface Events1Props {
@@ -10,16 +11,24 @@ export interface Events1Props {
     href?: string
     title?: string
     description?: string
+    published?: Date
+    when?: string
+    where?: string
   }[]
 }
 
 export default function ({ children, events }: Events1Props) {
+  const sortedEvents = events?.sort(
+    (a, b) =>
+      new Date(b.published || new Date()).getTime() -
+      new Date(a.published || new Date()).getTime()
+  )
   return (
     <section className="relative w-full py-16">
       <div className="mx-auto flex w-full max-w-screen-md flex-col px-4 md:px-12">
         {children && <Writeup size="4xl">{children}</Writeup>}
         <div className="flex flex-col gap-4 not-first:mt-16">
-          {events?.map(({ href, title, description }) => (
+          {sortedEvents?.map(({ href, title, description, when, where }) => (
             <a
               href={href}
               className="flex flex-col items-start gap-4 rounded-lg border p-6"
@@ -27,6 +36,18 @@ export default function ({ children, events }: Events1Props) {
             >
               <Heading as="h3">{title}</Heading>
               <Paragraph>{description}</Paragraph>
+              <ul className="flex flex-col gap-2 text-sm">
+                <li className="flex flex-row gap-2">
+                  <span className="text-primary w-16 font-medium">
+                    Wanneer:
+                  </span>
+                  <span>{when}</span>
+                </li>
+                <li className="flex flex-row gap-2">
+                  <span className="text-primary w-16 font-medium">Waar:</span>
+                  <span>{where}</span>
+                </li>
+              </ul>
             </a>
           ))}
         </div>
--- src/content/events/vipassana-meditatie-ochtend-leeuwarden.md
+++ src/content/events/vipassana-meditatie-ochtend-leeuwarden.md
@@ -1,4 +1,5 @@
 ---
+published: 2025-09-01
 title: vipassana meditatie ochtend Leeuwarden
 when: >-
   zaterdag ochtend, 1 keer per maand, van 10 tot 12.30 uur: 6 september, 4
@@ -9,6 +10,7 @@ description: >-
   vragen te stellen. Als je meditatie ervaring of een mindfulness training hebt
   gedaan: hartelijk welkom. Kom je voor het eerst, mail dan even vooraf.
 ---
+
 Vipassana meditatie ochtend Leeuwarden
 
 Eén keer per maand, op zaterdag ochtend is er een meditatie ochtend, van 10 tot 12.30 uur. We doen zit- en loop meditatie, er is de mogelijkheid om uit te wisselen en vragen te stellen.
